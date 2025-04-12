66178cd4d19d9fbf8d3e5ca61701242c76fea742
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-04-12 13:14:01 -0400

    helper functions for fetching

--- weather-news-app/src/components/Layout.jsx
+++ weather-news-app/src/components/Layout.jsx
@@ -3,12 +3,74 @@ import axios from 'axios';
 import {
   TextField,
   Button,
-  Card,
-  CardContent,
   Typography,
-  Grid,
-  CircularProgress,
 } from '@mui/material';
 
-const weatherKey = process.env.REACT_APP_WEATHER_API_KEY
-const newsKey = process.env.REACT_APP_NEWS_API_KEY
+const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
+const newsKey = process.env.REACT_APP_NEWS_API_KEY;
+
+export const Layout = () => {
+    const [location, setLocation] = useState('');
+    const [coords, setCoords] = useState(null);
+    const [weather, setWeather] = useState(null);
+    const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleLocationSearch = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get('https://api.openweathermap.org/geo/1.0/direct', {
+                params: {
+                    q: location,
+                    limit: 1,
+                    appid: weatherKey,
+                },
+            });
+
+            const [data] = response.data;
+            if (data) {
+                const {lat, lon} = data;
+                setCoords({lat, lon});
+            }
+        } catch (error) {
+            console.error('Error fetching geolocation:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        const getWeather = async () => {
+            if(!coords) return;
+        
+            try {
+                const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall', {
+                    params: {
+                        lat: coords.lat,
+                        lon: coords.lon,
+                        exclude: 'alerts',
+                        units: 'metric',
+                        appid: weatherKey,
+                    },
+                });
+                setWeather(response.data);
+            } catch (error) {
+                console.error('Error fetching weather', error);
+            }
+        };
+        const getNews = async () => {
+            try {
+              const response = await axios.get(
+                'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${newsKey}'
+              );
+              setNews(response.data.results.slice(0, 5));
+            } catch (error) {
+              console.error('Error fetching news:', error);
+            }
+          };
+
+          getWeather();
+          getNews();
+        }, [coords]);
+}
+
